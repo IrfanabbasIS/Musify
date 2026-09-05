2517db696934020f084d83d5eb07518fc81c4217
IrfanabbasIS <irfanabbas7757@gmail.com>
2026-09-05 17:43:55 +0530

    Added

--- com.bd.musify/src/main/java/com/bd/musify/serviceImpl/AuthServiceImpl.java
+++ com.bd.musify/src/main/java/com/bd/musify/serviceImpl/AuthServiceImpl.java
@@ -2,7 +2,6 @@ package com.bd.musify.serviceImpl;
 
 import java.security.SecureRandom;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,8 +24,7 @@ import com.bd.musify.service.EmailService;
 import com.bd.musify.util.JwtUtil;
 
 @Service
-public class AuthServiceImpl implements AuthService{
-
+public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private AppUserRepository appUserRepository;
@@ -43,92 +41,77 @@ public class AuthServiceImpl implements AuthService{
     @Override
     public MessageResponse registerUser(RegisterUserRequest request) {
 
-        if(appUserRepository.existsByEmail(request.getEmail()))
-        {
-            throw new EmailAlreadyExistsException("Email Already exists");     
-           }
-           String tempPassword=generateTemporaryPassword();
-           AppUser appUser=new AppUser();
-           appUser.setName(request.getName());
-           appUser.setEmail(request.getEmail());
-           appUser.setPassword(passwordEncoder.encode(tempPassword));
-           appUser.setRole(request.getRole() !=null ? request.getRole(): "USER");
-           appUserRepository.save(appUser);
-           emailService.sendWelcomeEmail(appUser.getEmail(), appUser.getName(), tempPassword);
-           return new MessageResponse("Account created successfully. A temporarry password has been sent to your email");
+        if (appUserRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException("Email Already exists");
+        }
+        String tempPassword = generateTemporaryPassword();
+        AppUser appUser = new AppUser();
+        appUser.setName(request.getName());
+        appUser.setEmail(request.getEmail());
+        appUser.setPassword(passwordEncoder.encode(tempPassword));
+        appUser.setRole(request.getRole() != null ? request.getRole() : "USER");
+        appUserRepository.save(appUser);
+        emailService.sendWelcomeEmail(appUser.getEmail(), appUser.getName(), tempPassword);
+        return new MessageResponse("Account created successfully. A temporarry password has been sent to your email");
     }
 
     @Override
-    public AppUserResponse loginUser(LoginUserRequest request) 
-    {
-        AppUser appUser=appUserRepository.findByEmail(request.getEmail())
-        .orElseThrow(()-> new InvalidCredentialsException("Invalid email or password"));
+    public AppUserResponse loginUser(LoginUserRequest request) {
+        AppUser appUser = appUserRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new InvalidCredentialsException("Invalid email or password"));
 
-
-        if(!passwordEncoder.matches(request.getPassword(), appUser.getPassword()))
-        {
+        if (!passwordEncoder.matches(request.getPassword(), appUser.getPassword())) {
             throw new InvalidCredentialsException("Invalid email or password");
         }
-        String accessToken =jwtUtil.generatedAccessToken(appUser.getId(), appUser.getName(), appUser.getEmail(), appUser.getRole());
-        String refreshToken=jwtUtil.generateRefreshToken(appUser.getId(), appUser.getEmail());
+        String accessToken = jwtUtil.generatedAccessToken(appUser.getId(), appUser.getName(), appUser.getEmail(),
+                appUser.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(appUser.getId(), appUser.getEmail());
         appUser.setRefreshToken(refreshToken);
         appUserRepository.save(appUser);
-
 
         return AppUserResponse.fromEntity(appUser, accessToken, refreshToken);
     }
 
     @Override
-    public AppUserResponse refreshAccessToken(RefreshTokenRequest request) 
-    {
-        String refreshToken=request.getRefreshToken();
-        String email=jwtUtil.extractEmail(refreshToken);
-        if(!jwtUtil.isRefreshToken(refreshToken))
-        {
+    public AppUserResponse refreshAccessToken(RefreshTokenRequest request) {
+        String refreshToken = request.getRefreshToken();
+        String email = jwtUtil.extractEmail(refreshToken);
+        if (!jwtUtil.isRefreshToken(refreshToken)) {
             throw new InvalidTokenException("Invalid token type");
 
         }
-        AppUser appUser=appUserRepository.findByRefreshToken(refreshToken)
-        .orElseThrow(()-> new InvalidTokenException("Invalid refresh token"));
-        if(!jwtUtil.validateToken(refreshToken, email))
-        {
+        AppUser appUser = appUserRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new InvalidTokenException("Invalid refresh token"));
+        if (!jwtUtil.validateToken(refreshToken, email)) {
             throw new TokenExpiredException("Refresh Token expired or invalid");
         }
 
-        String newAccessToken=jwtUtil.generatedAccessToken(appUser.getId(), appUser.getName(), appUser.getEmail(), appUser.getRole());
+        String newAccessToken = jwtUtil.generatedAccessToken(appUser.getId(), appUser.getName(), appUser.getEmail(),
+                appUser.getRole());
         return AppUserResponse.fromEntity(appUser, newAccessToken, refreshToken);
     }
 
-     @Override
-    public MessageResponse forgotPassword(ForgotPasswordRequest request) 
-    {
-        AppUser appUser=appUserRepository.findByEmail(request.getEmail())
-        .orElseThrow(()-> new ResourceNotFoundException("User not found with email:" +request
-            .getEmail()
-        ));
-        String tempPassword=generateTemporaryPassword();
+    @Override
+    public MessageResponse forgotPassword(ForgotPasswordRequest request) {
+        AppUser appUser = appUserRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with email:" + request
+                        .getEmail()));
+        String tempPassword = generateTemporaryPassword();
         appUser.setPassword(passwordEncoder.encode(tempPassword));
         appUserRepository.save(appUser);
         emailService.sendCredentials(appUser.getEmail(), appUser.getName(), tempPassword);
         return new MessageResponse("Temporary password has been sent to your register email");
-        
+
     }
 
-
     private String generateTemporaryPassword() {
-        String chars="KDKJSDKJHKJHASKDDKasdbvkbovuwercWDKAC830948hxlj8309lla93849!@#$%^&*";
-        SecureRandom random=new SecureRandom();
-        StringBuilder password=new StringBuilder(10);
-        for(int i=0; i<10; i++)
-        {
+        String chars = "KDKJSDKJHKJHASKDDKasdbvkbovuwercWDKAC830948hxlj8309lla93849!@#$%^&*";
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(10);
+        for (int i = 0; i < 10; i++) {
             password.append(chars.charAt(random.nextInt(chars.length())));
         }
         return password.toString();
     }
-
-   
-
-    
-    
 
 }
